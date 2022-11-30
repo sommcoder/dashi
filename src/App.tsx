f8581509f968420e06fa0bd745aec12716b401de
Brian Davies <brian.davies589@gmail.com>
2022-11-30 11:13:09 -0500

    got rid of some unnecessary code

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,11 @@
 import PageWindow from "./components/PageWindow/PageWindow";
 
-import { css } from "styled-components";
-import React, { FC } from "react";
-
 function App() {
   return (
-    <StyledApp className="App">
+    <div className="App">
       <PageWindow />
-    </StyledApp>
+    </div>
   );
 }
-
-const StyledApp = css`
-  border: 2px solid black;
-`;
 
 export default App;
