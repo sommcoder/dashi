c3ddbec0beeeb9ea73870deec9b3f8dda3f335ad
Brian Davies <brian.davies589@gmail.com>
2023-09-10 22:22:19 -0400

    Fixed some aesthetic concerns.

--- server/app.js
+++ server/app.js
@@ -3,14 +3,14 @@ dotenv.config();
 import express from "express";
 import cors from "cors";
 
-// import mongoose from "mongoose";
-
 import { router as tableRoutes } from "./routes/table-routes.js";
 
-// const username = encodeURIComponent(process.env.DB_USERNAME);
-// const password = encodeURIComponent(process.env.DB_PASSWORD);
-// const clusterName = encodeURIComponent(process.env.CLUSTER);
-// const URI = `mongodb+srv://${username}:${password}@${clusterName}.mluyyks.mongodb.net/?retryWrites=true&w=majority`;
+/*
+ 
+postgresql://postgres:[YOUR-PASSWORD]@db.ojzvtcddcayocgplssyn.supabase.co:5432/postgres
+ 
+*/
+
 const PORT = process.env.PORT || 3000;
 
 const app = express();
@@ -24,11 +24,6 @@ app.get("/", cors(), (_, res) => {
   res.send("we're connected"); // sends to client
 });
 
-// mongoose
-//   .connect(URI)
-//   .then(() => {
 app.listen(PORT, () =>
   console.log(`server is running on http://localhost:${PORT}`)
 );
-// })
-// .catch((err) => console.log(err));
