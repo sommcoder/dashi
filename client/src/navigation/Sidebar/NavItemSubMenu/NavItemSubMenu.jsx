2084058efff0217561de7d1d1ee72544031dd519
Brian Davies <brian.davies589@gmail.com>
2023-09-13 17:18:34 -0400

    Just trying to figure out this opacity transition to make the submenu accordion transition look more smooth!

--- client/src/navigation/Sidebar/NavItemSubMenu/NavItemSubMenu.jsx
+++ client/src/navigation/Sidebar/NavItemSubMenu/NavItemSubMenu.jsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 export default function NavItemSubMenu({ path, subMenu, navBarObj, menu }) {
   console.log("navBarObj:", navBarObj);
   return (
-    <div className="link-label menu-item submenu-item ">
+    <div className="submenu-item-row">
       <Link
         to={path}
         className={`link-label submenu-link ${navBarObj[menu] ? "active" : ""}`}
