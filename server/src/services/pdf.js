f1fbbe6dc6a09659d4d49d70618d9d759565e37f
Brian Davies <brian.davies589@gmail.com>
2023-10-10 13:44:55 -0400

    not sure why pdf-parse isnt working

--- server/src/services/pdf.js
+++ server/src/services/pdf.js
@@ -1,21 +1,43 @@
-import { fs } from "node:fs";
+import fs from "fs";
 import PDFParser from "pdf-parse";
-import * as path from "../../test/pdf/Amazon - CA333K74RE2I.pdf";
 
-/*
- 
-1) first handle client-side validation. Is the file a pdf?
-2) send a POST request to the server with the pdf as the payload
- 
-*/
+const file = "../../test/pdf/amazon.pdf";
 
-async function extractTextFromPDF(path) {
-  const pdfBuffer = fs.readFileSync(path);
-  const options = {};
-  const data = await PDFParser(pdfBuffer, options);
-  console.log("data.text:", data.text);
-  return data.text;
+// Function to read and parse a PDF file
+async function parsePDF(file) {
+  try {
+    // Read the PDF file using fs.promises
+    const dataBuffer = await fs.promises.readFile(file);
+
+    // Create a PDFParser instance
+    const pdfParser = new PDFParser(dataBuffer);
+
+    // Parse the PDF content
+    await pdfParser.load();
+
+    // Get the text content from the PDF
+    const text = pdfParser.getText();
+
+    // You can do whatever you want with the extracted text here
+    console.log("text:", text);
+
+    return text;
+  } catch (error) {
+    console.error("Error parsing PDF:", error);
+  }
 }
+
+// Usage
+const pdfFilePath = "path-to-your-pdf-file.pdf";
+parsePDF(pdfFilePath);
+
+// async function extractTextFromPDF(path) {
+//   const pdfBuffer = fs.readFileSync(path);
+//   const options = {};
+//   const data = await PDFParser(pdfBuffer, options);
+//   console.log("data.text:", data.text);
+//   return data.text;
+// }
 
 /*
  
@@ -24,5 +46,6 @@ async function extractTextFromPDF(path) {
 3) convert the rest of pdf to png
 4) feed the png to tesseract OCR to get text in the right order
  
-*/
 // to extract the text in the correct order on a pdf it may be easier to convert pdf to image
+
+*/
