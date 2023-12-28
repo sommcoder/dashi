7b919803ebc5d6a88fb35aa750de39a5bff193ce
Brian Davies <brian.davies589@gmail.com>
2023-12-28 14:36:05 -0500

    still working on figuring out how to make a call to Document AI. I was able to figure out how to write and read the files to the fs however node is throwing an error that the file does not exist..

--- server/src/routes/files.js
+++ server/src/routes/files.js
@@ -1,6 +1,9 @@
 const db = require("../db/db-client.js");
 const fs = require("node:fs");
+const path = require("node:path");
 const util = require("node:util");
+const { pipeline } = require("node:stream");
+const { documentRequest } = require("../services/doc-ai-client.js");
 
 /*
 Streams:
@@ -13,90 +16,51 @@ Pipes:
 - In Node.js connects a readable stream to a writable stream
 - This enables "chaining"
 */
-const { pipeline } = require("node:stream");
-
-const { documentRequest } = require("../services/doc-ai-client.js");
 
 // takes a function as an input and returns a version of the same that returns a promise instead of a callback
-const pumpAsync = util.promisify(pipeline);
+const pump = util.promisify(pipeline);
 
 module.exports = function fileRoutes(fastify, options, done) {
   fastify.post("/files", async (req, reply) => {
     try {
-      // accepts file(s) and sends files to Document AI API for parsing
-      // uses the returned object to create an array
-      // uses that array to INSERT INTO our DB
-
+      // HOW DO WE DETERMINE IF THERE ARE MULTIPLE FILES IN THE REQ.BODY?? do we add another K/V in the req.body?
       const data = await req.file();
-      // we get req.file() thanks to the multi-part plugin
 
-      console.log(
-        "DATA FILE:",
-        data.file,
-        "DATA FIELDS:",
-        data.fields,
-        "DATA FIELDNAME:",
-        data.fieldname,
-        "DATA FILENAME:",
-        data.filename,
-        "DATA ENCODING:",
-        data.encoding,
-        "DATA MIMETYPE:",
-        data.mimetype
-      );
+      // await data.toBuffer(); // Buffer
 
-      const mimeType = data.mimetype;
-      console.log("mimeType:", mimeType);
+      const filename = data.filename;
+      const filePath = path.join("../server/src/uploads/", filename);
+      await pump(data.file, fs.createWriteStream(filePath));
 
-      if (mimeType === "text/csv") {
-        // use csv library call to save on costs.. however if it doesn't work MOST of the time for csv files.
-        // Probably can only handle simple header/row structured csv files. if there is any meta data detected, just send to document AI!
-        // metadata ? send to Document AI : csv Library;
-        // MAYBE .csv parsing is best handled on the client???
-      }
+      // if  single file:
+      const res = await documentRequest(filePath, data.mimetype);
+      console.log("res:", res);
+      // if multiple files, batch processing:
+      // await batchProcessDocument();
+      // if (data.mimeType === "text/csv") {
+      //   // use csv library call to save on costs.. however if it doesn't work MOST of the time for csv files than there would be no point in implementing it
+      //   // metadata ? Document AI : CSV Parse;
+      // }
 
-      if (
-        mimeType ===
-          "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" ||
-        mimeType === "application/vnd.ms-excel"
-      ) {
-        // convert into CSV (how?)
-        // and then process as CSV using a csv library.
-        // is there a way to save these
-      }
+      // if (
+      //   data.mimeType ===
+      //     "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" ||
+      //   data.mimeType === "application/vnd.ms-excel"
+      // ) {
+      //   // convert into CSV (how?)
+      //   // and then process as CSV using a csv library.
+      //   // is there a way to save these
+      // }
 
-      if (
-        mimeType === "application/pdf" ||
-        mimeType === "image/png" ||
-        mimeType === "image/jpeg"
-      ) {
-        /*
-   There is one concept to keep in mind: someone must consume the uploaded file. The consumer could be:
-
-   - Your implementation
-   - The plugin itself calling the toBuffer() method
-   */
-
-        /*
-    
-   ***************** I think I need to create a write stream and WHEN done, send that file path to the documentRequest function below
-
-
-   ** maybe I need to install Multer library to handle the multipart data type we are receiving.
-
-
-   we need to READ the file into memory
-    
-   */
-        const stream = fs.createWriteStream(data.file);
-        console.log("stream:", stream);
-        // The file is being ADDED to my server...?
-
-        // call GCP Document AI:
-        documentRequest(data.file, mimeType);
-      } else {
-        // return error. Not supported file format. This is also performed on the client however would be good to have a counter measure here too!
-      }
+      // if (
+      //   data.mimeType === "application/pdf" ||
+      //   data.mimeType === "image/png" ||
+      //   data.mimeType === "image/jpeg"
+      // ) {
+      // } else {
+      //   // return error. Not supported file format.
+      //   // This is also performed on the client however would be good to have a counter measure here too!
+      // }
 
       reply
         .code(200)
@@ -109,9 +73,6 @@ module.exports = function fileRoutes(fastify, options, done) {
     }
   });
 
-  /**
-   *
-   */
   fastify.get("/files", async (req, reply) => {
     try {
       // get all files by account id
--- server/src/services/doc-ai-client.js
+++ server/src/services/doc-ai-client.js
@@ -10,76 +10,89 @@ const location = "us"; // Format is 'us' or 'eu'
 const invoiceProcessorId = "b583473c7f1573e4"; // invoice parser
 // const expenseProcessorId = "3bfb41454f01c7be"; // expense parser
 
+// determining which parser to use will be established by the CLIENT sending the POST request. We may need to add tableType or pageType into the req.body
+// If it's on the Invoice Page, it's an invoice, if it's on the expense page, it's an expense!
+
 const client = new DocumentProcessorServiceClient();
 
-async function documentRequest(file, mimeType) {
+async function documentRequest(filePath, mimeType) {
   /*
-- FileType is determined by default based on the page or TableType the user dropped the file on. If sales, drop in sales table/page
- 
-*/
+    - FileType is determined by default based on the page or TableType the user dropped the file on. If sales, drop in sales table/page
+     
+    */
 
   // The full resource name of the processor, e.g.:
   // projects/project-id/locations/location/processor/processor-id
   // You must create new processors in the Cloud Console first
-  const name = `projects/${projectId}/locations/${location}/processors/${invoiceProcessorId}`;
 
-  // we need to READ the file into memory
+  try {
+    const name = `projects/${projectId}/locations/${location}/processors/${invoiceProcessorId}`;
 
-  // Convert the image data to a Buffer and base64 encode it.
-  const encodedImage = Buffer.from(file).toString("base64");
+    // Read the file into memory.
+    console.log("filePath:", filePath);
+    const imageFile = await fs.readFile(filePath);
 
-  // prepare the request to send to Document AI API
-  const request = {
-    name,
-    skipHumanReview: true, // no "human-in-the-middle"
-    rawDocument: {
-      content: encodedImage,
-      mimeType: mimeType,
-    },
-  };
+    // Convert the image data to a Buffer and base64 encode it.
+    const encodedImage = Buffer.from(imageFile).toString("base64");
 
-  // Recognizes text entities in the PDF document
-  //////// here's the issue:
-  const [result] = await client.processDocument(request);
+    // prepare the request to send to Document AI API
+    const request = {
+      name,
+      skipHumanReview: true, // no "human-in-the-middle"
+      rawDocument: {
+        content: encodedImage,
+        mimeType: mimeType,
+      },
+    };
+    console.log("request:", request);
 
-  console.log("result:", result);
+    // Recognizes text entities in the PDF document
+    // !     here's the issue:
+    // !    returns does not exist, or it is not a file.
+    // ! How am I able to READ the file above if it does not exist????
+    const [result] = await client.processDocument(request);
 
-  const { document } = result;
+    console.log("result:", result);
 
-  // Get all of the document text as one big string
-  const { text } = document;
-  console.log("text:", text);
+    const { document } = result;
 
-  // Extract shards from the text field
-  const getText = (textAnchor) => {
-    if (!textAnchor.textSegments || textAnchor.textSegments.length === 0) {
-      return "";
+    // Get all of the document text as one big string
+    const { text } = document;
+    console.log("text:", text);
+
+    // Extract shards from the text field
+    const getText = (textAnchor) => {
+      if (!textAnchor.textSegments || textAnchor.textSegments.length === 0) {
+        return "";
+      }
+
+      // First shard in document doesn't have startIndex property
+      const startIndex = textAnchor.textSegments[0].startIndex || 0;
+      const endIndex = textAnchor.textSegments[0].endIndex;
+
+      return text.substring(startIndex, endIndex);
+    };
+
+    // Read the text recognition output from the processor
+    console.log("The document contains the following paragraphs:");
+    console.log("document.pages:", document.pages);
+    const [page1] = document.pages;
+    const { paragraphs } = page1;
+
+    let paragraphText = "";
+    for (const paragraph of paragraphs) {
+      paragraphText = getText(paragraph.layout.textAnchor);
+      console.log(`Paragraph text:\n${paragraphText}`);
     }
 
-    // First shard in document doesn't have startIndex property
-    const startIndex = textAnchor.textSegments[0].startIndex || 0;
-    const endIndex = textAnchor.textSegments[0].endIndex;
+    const responseObj = {
+      paragraphText: paragraphText,
+    };
 
-    return text.substring(startIndex, endIndex);
-  };
-
-  // Read the text recognition output from the processor
-  console.log("The document contains the following paragraphs:");
-  console.log("document.pages:", document.pages);
-  const [page1] = document.pages;
-  const { paragraphs } = page1;
-
-  let paragraphText = "";
-  for (const paragraph of paragraphs) {
-    paragraphText = getText(paragraph.layout.textAnchor);
-    console.log(`Paragraph text:\n${paragraphText}`);
+    return responseObj;
+  } catch (err) {
+    console.log("document-ai-client error:", err.message);
   }
-
-  const responseObj = {
-    paragraphText: paragraphText,
-  };
-
-  return responseObj;
 }
 
 module.exports = {
